fa6f3cb2c8446b4bf19c7ba137cdd999246a9420
nayrannery <88459690+nayrannery@users.noreply.github.com>
2021-09-09 15:16:12 -0300

    V1.1

--- main.js
+++ main.js
@@ -27,20 +27,20 @@ class ToList extends List {
        
 }
 
-/*
+
 const tolist = new ToList()
 
 document.getElementById('novotodo').onclick = function () {
     tolist.add('Joao');
     tolist.getUsuario();
-}*/
+}
 
-const nome = {primeiro_mome: "lucas"};
+//const nome = {primeiro_mome: "lucas"};
 //console.log(ToList.soma(1,2));
 //var nome = "lucas"; //pode modificar
 //const nome = "lucas";
-nome.primeiro_mome="joao";
-console.log(nome);
+//nome.primeiro_mome="joao";
+//console.log(nome);
 
 function teste(x){
     let y=4
